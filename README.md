3f85f88c839f5f15972118c66854cda288eb8ee4
MatheusZaboti <matheuszamboti1@gmail.com>
2024-08-22 16:49:48 -0300

    atividade 01

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# p-proj
+<<<<<<< HEAD
+=======
+# primeiro-projeto
+primeira aula de git da turma do Claudinas
+
+- git clone <caminho do reporsitorio>
+- git status => verificar o status do reporsitorio
+- git add . => adicionar todos os arquivos no repositorio
+- git commit -m "nome da ação" => rastreio da ação de desenvolvimento 
+- git push => empurrar para o git
+>>>>>>> c7dd5067c074d2af2e556400139f654d89600a9b
---
+++ atividade01/script.js
@@ -0,0 +1,28 @@
+function soma(){
+    var numero01 = document.getElementById("number1").valueAsNumber
+    var numero02 = parseFloat (document.getElementById("number2").value)
+    var resposta = document.getElementById("respSoma")
+    resposta.textContent = numero01 + numero02
+
+}
+
+function sub(){
+    var numero01 = parseFloat(document.getElementById("number3").value)
+    var numero02 = parseFloat(document.getElementById("number4").value)
+    var resposta = document.getElementById("respSub")
+    resposta.textContent = numero01 - numero02
+}
+function Div(){
+
+    var numero01 = parseFloat(document.getElementById("number5").value)
+    var numero02 = parseFloat(document.getElementById("number6").value)
+    var resposta = document.getElementById("respDiv")
+    resposta.textContent = numero01 / numero02
+}
+function Mult(){
+
+    var numero01 = parseFloat(document.getElementById("number7").value)
+    var numero02 = parseFloat(document.getElementById("number8").value)
+    var resposta = document.getElementById("respmult")
+    resposta.textContent = numero01 / numero02
+}   
